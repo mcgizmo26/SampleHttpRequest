0554a93a4347a2280c7997f91f4e7eb755e52c20
mcgizmo26 <lonnie.mcgill@smartshyp.com>
2021-05-23 19:13:46 -0600

    Working through  Go Http request

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, err := http.Get("http://google.com")
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	// resp.Body.Read()
+
+	fmt.Println(resp)
+}
